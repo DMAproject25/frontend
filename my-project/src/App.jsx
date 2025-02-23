5f73fc6ed65642677c33f14eeaaea132c09bed3a
letyaga_gang <miheyka2005@gmail.com>
2025-02-23 22:48:18 +0300

    Секретик

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>=зло и негатив=</h1>
+      <h1>=Пиздешь и наебалово=</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
