a95d46b36b564a8f511ee019454618c5176548c5
letyaga_gang <miheyka2005@gmail.com>
2025-03-01 23:46:44 +0300

    Накат библиотек

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,37 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import "antd/dist/reset.css";
+// App.jsx
 
+import { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="phone-container">
+      <div className="app-content">
+        {/* Ваше содержимое приложения */}
+        <h1 className= "pohuy">Мой кент</h1>
+        <p>Это контейнер размером с мобильный экран</p>
       </div>
-      <h1>=Пиздешь и наебалово=</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+    
+  );
 }
 
-export default App
+export default App;
